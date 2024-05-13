968ece1797099311db4ffb1587d3b5e7f78ce8bb
Jamshidbek <jamshidbekollanazarov@gmail.com>
2024-05-13 16:15:21 +0500

    LOGIN REGISTER AND WELCOME FRONTEND CHANGED

--- calorie/settings.py
+++ calorie/settings.py
@@ -122,7 +122,21 @@ LOGIN_REDIRECT_URL = "home"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# Base directory of your project
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Optional: specify the directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Path to where you keep your static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- calorie/urls.py
+++ calorie/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),  # your application's urls
     path('', include('pages.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
